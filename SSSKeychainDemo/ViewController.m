d6a2f47d56944ea9d63d5cdb4b41d272091b9f5d
ComAnvei <comanvei@ComAnveideiMac.local>
2016-05-25 18:07:22 +0800

    SSKeychain加密用@“MyService”

--- SSSKeychainDemo/ViewController.m
+++ SSSKeychainDemo/ViewController.m
@@ -23,10 +23,10 @@
     NSString *userName = @"cerastes";
     NSString *password = @"新浪科技讯 北京时间5月25日早间消息，美国超级高铁公司Hyperloop Transportation Technologies(HTT)周二公布了一种智能材料。这种材料集成了传感器，将被用于制造超级高铁的车辆。HTT表示，这种材料主要由轻量级的碳纤维构成，强度超过任何钢材料10倍，而且比铝合金还轻，被称作“Vibranium”(汎合金，注：与漫画人物“美国队长”盾牌的材料同名)。利用这种材料可以降低车辆长途行驶所需的能耗。此外，材料内集成的传感器可以测量车辆的信息，并将其回传给运营中心。这些传感器可以实时读取车辆的温度、稳定度和完整度，确保安全和性能。Hyperloop超级高铁的时速最高可以达到700英里(约合1127公里)，最先由特斯拉CEO伊隆·马斯克(Elon Musk)提出。目前，有两家公司试图将马斯克的设想变为现实，而HTT是其中之一。另一家名为Hyperloop One的公司本月早些时候在内华达州的沙漠中展示了测试轨道的初步工作。HTT尚未公布类似的磁力推进系统。该公司目前专注于安全性方面的创新。该公司计划利用两层智能材料去建造车辆，因此即使外层材料损毁，车辆仍可以安全运行。　HTT CEO德科·阿尔伯恩(Derk Ahlborn)表示：“安全性是我们系统最重要的一个方面。”(维金)";
     NSString *bundleID = [NSBundle mainBundle].bundleIdentifier;
-    [SSKeychain setPassword:@"111" forService:bundleID account:@"ni"];
-    [SSKeychain setPassword:password forService:bundleID account:@"wo"];
+    [SSKeychain setPassword:@"111" forService:@"MyService" account:@"ni"];
+    [SSKeychain setPassword:password forService:@"MyService" account:@"wo"];
     [SSKeychain setPassword:@"333" forService:bundleID account:@"ta"];
-    
+    [SSKeychain deletePasswordForService:bundleID account:@"ni"];
     NSString *passwords = [SSKeychain passwordForService:bundleID account:@"wo"];
     NSLog(@"SSkeychain解密=====%@",passwords);
     
